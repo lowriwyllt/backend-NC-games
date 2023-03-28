edaa4a40d0de84e63d2582b026e56827d7672a9e
lowriwyllt <lowriwyllt@live.co.uk>
2023-03-28 10:46:30 +0100

    refactoring and organising my errors

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const {
   getReviewById,
   getReviews,
 } = require("./controllers/reviews.controller");
-const { psqlErrors } = require("./errors");
+const { errorPSQL400s, errorCustomised, error500 } = require("./errors");
 
 const app = express();
 
@@ -12,15 +12,9 @@ app.get("/api/categories", getCategories);
 app.get("/api/reviews/:review_id", getReviewById);
 app.get("/api/reviews", getReviews);
 
-app.use((err, req, res, next) => {
-  if (err.code) {
-    //PSQL Errors
-    psqlErrors(err.code, res);
-  } else if (err.status && err.msg) {
-    //My personalised error messages
-    res.status(err.status).send({ msg: err.msg });
-  }
-});
+app.use(errorPSQL400s);
+app.use(errorCustomised);
+app.use(error500);
 
 app.use((req, res, next) => {
   res.status(404).send({ msg: "Path does not exist" });
