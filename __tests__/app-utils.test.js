0d1caeb47a75fbc892cb2da4b28c52a1355bff84
lowriwyllt <lowriwyllt@live.co.uk>
2023-03-28 15:14:13 +0100

    returns a reject promise instead, but no way to test for it until in app

--- __tests__/app-utils.test.js
+++ __tests__/app-utils.test.js
@@ -8,17 +8,6 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 describe("checkColumnExists", () => {
-  it("return an object { status: 404, msg: `${column} does not exist` } if doesn't exist", () => {
-    return checkColumnExists("reviews", "review_id", "900000").then(
-      (response) => {
-        console.log(response);
-        expect(response).toEqual({
-          status: 404,
-          msg: `review_id does not exist`,
-        });
-      }
-    );
-  });
   it("return nothing, i.e. undefined if does exist", () => {
     return checkColumnExists("reviews", "review_id", 3).then((response) => {
       expect(response).toEqual(undefined);
