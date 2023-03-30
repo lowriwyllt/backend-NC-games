70df3a4ca00df21e711bd31914e27358b06fbe0e
lowriwyllt <lowriwyllt@live.co.uk>
2023-03-30 20:48:24 +0100

    add a /api/users/:username path to get the user data

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -1,9 +1,21 @@
-const { fetchUsers } = require("../models/users.model");
+const { fetchUsers, fetchUser } = require("../models/users.model");
 
 exports.getUsers = (req, res, next) => {
   fetchUsers()
     .then((users) => {
       res.status(200).send({ users });
+    })
+    .catch((err) => next(err));
+};
+
+exports.getUser = (req, res, next) => {
+  const { username } = req.params;
+  fetchUser(username)
+    .then((user) => {
+      if (user === undefined) {
+        return Promise.reject({ status: 404, msg: "username does not exist" });
+      }
+      res.status(200).send({ user });
     })
     .catch((err) => next(err));
 };
